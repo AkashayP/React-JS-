0adf2b3478a18a1d6e9e857200034260cd34e6e3
AkashayP <akshayprakashsingh2004@gmail.com>
2024-10-03 15:04:15 +0530

    home page extended

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./HomePage.css";
 import webVideo from "../assets/webVideo.mp4";
 import Frame_35 from "../assets/Frame_35.png";
-import arrow from "../assets/arrow.png";
+import arrow from "../assets/arrow.svg";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
@@ -21,24 +21,12 @@ const HomePage = () => {
         </button>
         <h1>Empower Your Future with Coding Skills</h1>
         <p>
-          With our online coding courses, you can learn at your own pace, from
-          anywhere in the world, and get access to a wealth of resources,
-          including hands-on projects, quizzes, and personalized feedback from
-          instructors.
+          With our online coding courses, you can learn at your own pace, from anywhere in the world, and get access to a wealth of resources, including hands-on projects, quizzes, and personalized feedback from instructors.
         </p>
       </div>
-
       <div className="button-group">
-        <button className="shrink-button">
-          <Link className="link" to="/signup">
-            Learn More
-          </Link>
-        </button>
-        <button className="shrink-button2">
-          <Link className="link" style={{ textDecoration: "none" }} to="/">
-            Book a Demo
-          </Link>
-        </button>
+        <button className="shrink-button"><Link className="link" to="/signup">Learn More</Link></button>
+        <button className="shrink-button2"><Link className="link" to="/">Book a Demo</Link></button>
       </div>
 
       <div className="video-container">
@@ -46,7 +34,6 @@ const HomePage = () => {
           <source src={webVideo} type="video/mp4" />
         </video>
         {/* <img src={Frame_35}alt="bg_image" className="bg-image" /> */}
-        <h1>hello</h1>
       </div>
     </div>
   );
